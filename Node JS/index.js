deaf67b10cc51157527a996a38ea4091ef044c5a
Abdelrahman Salem <67508048+abdelrahman-20@users.noreply.github.com>
2024-01-20 12:40:39 +0200

    Updating Files

---
+++ Node JS/index.js
@@ -0,0 +1,8 @@
+const superHeroes = require("superheroes");
+const superVillains = require("supervillains");
+
+let mySuperHero = superHeroes.random();
+let myVillain = superVillains.random();
+
+console.log(mySuperHero);
+console.log(myVillain);
