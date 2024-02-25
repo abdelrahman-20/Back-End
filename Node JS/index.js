e8454dba29cd7312abe38a49cf1867cfde4cfec9
abdelrahman-20 <67508048+abdelrahman-20@users.noreply.github.com>
2024-02-25 12:13:39 +0200

    Update Project Files

--- Node JS/index.js
+++ Node JS/index.js
@@ -1,9 +1,18 @@
-const fs = require("fs");
+// const fs = require("fs");
+// const superHeroes = require("superheroes");
+// const superVillains = require("supervillains");
+
+// let mySuperHero = superHeroes.random();
+// let myVillain = superVillains.random();
+
+// console.log(mySuperHero);
+// console.log(myVillain);
+
 const superHeroes = require("superheroes");
-const superVillains = require("supervillains");
+const supervillains = require("supervillains");
 
 let mySuperHero = superHeroes.random();
-let myVillain = superVillains.random();
+let myVillain = supervillains.random();
 
 console.log(mySuperHero);
 console.log(myVillain);
