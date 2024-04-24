c557b9d7a72be76633fe5d8486b2fedbf547c026
Abdelrahman Salem <67508048+abdelrahman-20@users.noreply.github.com>
2024-04-24 02:03:19 +0200

    Call-Backs In Node-JS

--- Node Farm/index.js
+++ Node Farm/index.js
@@ -1,52 +1,36 @@
-// const fs = require("fs");
-// const http = require("http");
-
-// //* ------------------------------------------------------------------------------------------ *//
-// // Blocking, Synch Way: //
-// // let textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-// // let textOutput = `This is the information we have: ${textInput}\nCreated On ${Date.now()}`;
-// // fs.writeFileSync("./txt/output.txt", textOutput, "utf-8");
-// // console.log("File Written Successfully");
-
-// // Non-Blocking, Asynch Way: //
-// // fs.readFile(`./txt/start.txt`, `utf-8`, (err, data1) => {
-// // 	fs.readFile(`./txt/${data1}.txt`, `utf-8`, (err, data2) => {
-// // 		console.log(data2);
-// // 		fs.readFile(`./txt/append.txt`, `utf-8`, (err, data3) => {
-// // 			console.log(data3);
-// // 			fs.writeFile(
-// // 				`./txt/final.txt`,
-// // 				`${data2}\n${data3}`,
-// // 				"utf-8",
-// // 				(err) => {
-// // 					console.log("Your File Has Been Written 😂😂");
-// // 				}
-// // 			);
-// // 		});
-// // 	});
-// // });
-// // console.log("Will Read File");
-
-// //* ------------------------------------------------------------------------------------------ *//
-// //* Server:
-
-// const server = http.createServer((req, res) => {
-// 	res.end("<h1>Hello From The Server Side</h1>");
-// });
-
-// server.listen(3000, "localhost", () => {
-// 	console.log("Server is Started");
-// });
-
-//* ------------------------------------------------------------------------------------------ *//
-
 const fs = require("fs");
 
-// const textInput = fs.readFile("./txt/input.txt", "utf-8");
+// --------------------------------------------------------------
+// Synchronous, Blocking Way:
+// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textInput);
-fs.readFile("./txt/input.txt", "utf-8", (err, data) => {
-	if (err) console.error(err);
-	console.log(data);
-});
 
-console.log("HELLO FROM JS FILE !!");
+// --------------------------------------------------------------
+// Asynchronous, Non-Blocking Way:
+// Call-Back HELL !!
+
+// fs.readFile("./txt/start111.txt", "utf-8", (err, data1) => {
+// 	if (err) console.error("Error Reading The File 🔥🔥");
+
+// 	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+// 		if (err) console.error("Error Reading The File 🔥🔥");
+// 		console.log(data2 + `\n`);
+
+// 		fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+// 			if (err) console.error("Error Reading The File 🔥🔥");
+// 			console.log(data3 + `\n`);
+
+// 			fs.writeFile(
+// 				"./txt/final.txt",
+// 				`${data2}\n\n${data3}`,
+// 				"utf-8",
+// 				(err) => {
+// 					if (!err) console.log("WRITE FILE SUCCESS ✅✅");
+// 				}
+// 			);
+// 		});
+// 	});
+// });
+// console.log("HELLO FROM JS FILE !!" + `\n`);
+
+// --------------------------------------------------------------
