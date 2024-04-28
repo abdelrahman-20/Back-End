2169d803c77f00cf5dee07276b0082860aaf1976
Abdelrahman Salem <67508048+abdelrahman-20@users.noreply.github.com>
2024-04-28 19:22:19 +0300

    Require and Import Modules, How To Create A Web Server, Templating

--- Node Farm/index.js
+++ Node Farm/index.js
@@ -30,109 +30,90 @@
 // });
 // console.log("HELLO FROM JS FILE !!" + `\n`);
 
-/**/
-// Creating A Web Server:
+/*
+ * Node Farm Project
+ */
 
-//* Declaring Variables & Used Modules *//
-const port = 3000;
+//* Using Modules *//
 const fs = require("fs");
 const url = require("url");
 const http = require("http");
+const slugify = require("slugify");
+const replaceTemplate = require("./modules/replaceTemplate");
+
+//* Declaring Variables *//
+const port = 3000;
 
 //* READING DATA FROM FILES *//
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/temp_card.html`,
+  "utf-8"
+);
+const tempProducts = fs.readFileSync(
+  `${__dirname}/templates/temp_product.html`,
+  "utf-8"
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/temp_overview.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
 
-const tempCard = fs.readFileSync(
-	`${__dirname}/templates/temp_card.html`,
-	"utf-8"
-);
-const tempProducts = fs.readFileSync(
-	`${__dirname}/templates/temp_product.html`,
-	"utf-8"
-);
-const tempOverview = fs.readFileSync(
-	`${__dirname}/templates/temp_overview.html`,
-	"utf-8"
-);
+const slugs = dataObject.map((el) => {
+  return slugify(el.productName, { lower: true });
+});
 
-/*
-ID
-PRODUCTNAME
-FROM
-NUTRIENTS
-QUANTITY
-PRICE
-DESCRIPTION
-IMAGE
-NOT_ORGANIC
-
-PRODUCT_CARDS
-*/
-
-//* Functions *//
-const replaceTemplate = (template, product) => {
-	let output = template.replace(/{%ID%}/g, product.id);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-	if (!product.organic)
-		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-	return output;
-};
+console.log(slugs);
 
 //* Creating The Server *//
 const server = http.createServer((req, res) => {
-	//! Get The Required Path (Route):
-	const pathName = req.url;
+  //? Get The Required Path (Route):
+  // const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-	//! Overview Page:
-	if (pathName === "/" || pathName === "/overview") {
-		const cardsHTML = dataObject
-			.map((el) => {
-				return replaceTemplate(tempCard, el);
-			})
-			.join("");
-		let output = tempOverview.replace(/{%PRODUCT_CARDS%}/, cardsHTML);
+  //? Overview Page:
+  if (pathname === "/" || pathname === "/overview") {
+    const cardsHTML = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    let output = tempOverview.replace(/{%PRODUCT_CARDS%}/, cardsHTML);
 
-		res.writeHead(200, {
-			"content-type": "text/html",
-		});
-		res.end(output);
-	}
-	//! Products Page:
-	else if (pathName === "/products") {
-		res.writeHead(200, {
-			"Content-type": "text/html",
-		});
-		res.end(`This is Products Page`);
-	}
-	//! API Page:
-	else if (pathName === "/api") {
-		// Response Header: "First"
-		res.writeHead(200, { "content-type": "application/json" });
-		// Response Content: "Second"
-		res.end(data);
-	}
-	//! ERROR Page:
-	else {
-		// Response Header: "First"
-		res.writeHead(404, {
-			"content-type": "text/html",
-			"custom-header": "Error, Can't Find This Page.",
-		});
-		// Response Content: "Second"
-		res.end(`<center><h1>Hello From Server Side</h1></center>`);
-	}
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.end(output);
+  }
+  //? Products Page:
+  else if (pathname === "/product") {
+    const product = dataObject[query.id];
+    const output = replaceTemplate(tempProducts, product);
+
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(output);
+  }
+  //? API Page:
+  else if (pathname === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  }
+  //? ERROR Page:
+  else {
+    // Response Header: "First"
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "custom-header": "Error, Can't Find This Page.",
+    });
+    // Response Content: "Second"
+    res.end(`<center><h1>Hello From Server Side</h1></center>`);
+  }
 });
 
 //* Starting The Server *//
 server.listen(port, "localhost", () => {
-	console.log(`Listening On Port: ${port}`);
+  console.log(`Listening On Port: ${port}`);
 });
