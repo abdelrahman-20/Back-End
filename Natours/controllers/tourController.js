e2a708877e6599bdb8c873576aa6f3d6c4e81d4f
Abdelrahman Salem <67508048+abdelrahman-20@users.noreply.github.com>
2024-06-08 00:37:22 +0300

    Finish MongoDB Section

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -66,7 +66,7 @@ exports.createTour = async (req, res) => {
         tour: newTour,
       },
     });
-  } catch (error) {
+  } catch (err) {
     res.status(400).json({
       status: "Failure",
       message: err,
@@ -154,7 +154,37 @@ exports.getTourStates = async (req, res) => {
 exports.getMonthlyPlan = async (req, res) => {
   try {
     const year = req.params.year * 1;
-    const plan = await Tour.aggregate([{}]);
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numberTourStart: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numberTourStart: -1 },
+      },
+    ]);
 
     res.status(200).json({
       status: "Success",
